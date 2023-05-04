21db3dd1ea13fdf576028f9c701bd47338b04798
Maria Tsantaki <Maria.Tsantaki@astro.up.pt>
2023-05-04 18:29:45 +0200

    Update rv_correction.py
    add wider limits for rv, only fits accepted

--- rv_correction.py
+++ rv_correction.py
@@ -1,84 +1,119 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  6 15:18:27 2022
+
+@author: maria.tsantaki
+"""
 from __future__ import print_function, division
 import numpy as np
 from astropy.io import fits as fits
 from PyAstronomy import pyasl
-from observations import read_obs_intervals, read_observations, snr
+from observations import read_observations, local_norm, read_raw_observations, eso_fits, snr
 import matplotlib.pyplot as plt
-
-def rv_correction(filename, velocity):
-    c = 299792.458 #km s-1
-    hdulist = fits.open(filename)
-    header = hdulist[0].header #read header
-    start_wave = header['CRVAL1'] #initial wavelenght
-    #print start_wave
-    step = header['CDELT1'] #step in wavelenght
-    #print step
-    # New starting wavelenght and step
-    relat_wave = start_wave * np.sqrt((1. - velocity/c)/(1. + velocity/c))
-    relat_step = step * np.sqrt((1. - velocity/c)/(1. + velocity/c))
-    #Change the header with the new values
-    header['CRVAL1'] = relat_wave
-    header['CDELT1'] = relat_step
-    header['CD1_1'] = relat_step
-    #print relat_wave
-    #print relat_step
-    #Save results
-    hdulist.writeto(filename[:-5]+"_rv.fits", overwrite=True)
-    hdulist.close()
-    return
+import argparse
 
 def read_template(fname):
     '''Choose the template.'''
-    SNR = snr(fname)
-    intervals = [5339.8, 5500.40]
-    intervals = [6500., 6800.]
-    tw, tf, d = read_observations(fname, intervals[0]-1.0, intervals[1]+1.0)
+    intervals = [5350., 6800.]
+    tw, tf, d = read_observations(fname, intervals[0], intervals[1])
     tf = 1.0 - tf
     return tw, tf
 
 def read_spec(fname):
     '''Read spectra.'''
     SNR = snr(fname)
-    intervals = [6500., 6800.]
-    dw, df, d = read_obs_intervals(fname, [intervals], SNR)
+    intervals = [5500., 6700.]
+    dw, df, d = local_norm(fname, intervals, SNR)
     df = 1.0 - df
     return dw, df
 
-def velocity_shift(dw, df, tw, tf):
+def velocity_shift(dw, df, tw, tf, plot=False):
     '''Carry out the cross-correlation.
-    The RV-range is -30 - +30 km/s in steps of 0.6 km/s.
-    The first and last 20 points of the data are skipped.'''
-    rv, cc = pyasl.crosscorrRV(dw, df, tw, tf, -40., 40., 0.01, skipedge=100)
+    The RV-range is -200 - +200 km/s in steps of 0.2 km/s.
+    The first and last 10 points of the data are skipped.'''
+    
+    rv, cc = pyasl.crosscorrRV(dw, df, tw, tf, -200., 200., 0.2, skipedge=10)
     # Find the index of maximum cross-correlation function
     maxind = np.argmax(cc)
     print("Cross-correlation function is maximized at dRV = ", rv[maxind], " km/s")
-    #plt.plot(rv, cc, 'bp-')
-    #plt.plot(rv[maxind], cc[maxind], 'ro')
-    #plt.show()
-    return rv[maxind]
+    v = rv[maxind]
+
+    if plot:
+        plt.plot(rv, cc, 'bp-')
+        plt.plot(rv[maxind], cc[maxind], 'ro')
+        plt.title('CCF')
+        plt.show()
+    return v
+
+def rv_correction(filename, velocity):
+    c = 299792.458 #km s-1
+    hdulist = fits.open(filename)
+    header = hdulist[0].header #read header
+    if 'PRODCATG' in header:
+        # ESO Product
+        if eso_fits(hdulist) is None:
+            hdulist.writeto(filename[:-5]+"_rv.fits", overwrite=True)
+            hdulist.close()
+        else:
+             wave, flux = eso_fits(hdulist)
+             start_wave = wave[0]
+             step = wave[1] - wave[0]
+             # New starting wavelenght and step
+             relat_wave = start_wave * np.sqrt((1. - velocity/c)/(1. + velocity/c))
+             relat_step = step * np.sqrt((1. - velocity/c)/(1. + velocity/c))
+             #Change the header with the new values
+             header['CRVAL1'] = relat_wave
+             header['CDELT1'] = relat_step
+             header['CD1_1'] = relat_step
+             hdulist.writeto(filename[:-5]+"_rv.fits", overwrite=True)
+             hdulist.close()
+
+    elif 'CRVAL1' in header:
+        start_wave = header['CRVAL1'] #initial wavelenght
+        step = header['CDELT1'] #step in wavelenght
+
+        # New starting wavelenght and step
+        relat_wave = start_wave * np.sqrt((1. - velocity/c)/(1. + velocity/c))
+        relat_step = step * np.sqrt((1. - velocity/c)/(1. + velocity/c))
+        #Change the header with the new values
+        header['CRVAL1'] = relat_wave
+        header['CDELT1'] = relat_step
+        header['CD1_1'] = relat_step
+        hdulist.writeto(filename[:-5]+"_rv.fits", overwrite=True)
+        hdulist.close()
+
+    else:
+        #Save results
+        hdulist.writeto(filename[:-5]+"_rv.fits", overwrite=True)
+        hdulist.close()
+    return
+
+def plot_rv(observed, rv):
+
+    wave, flux, delta_l, start_wave = read_raw_observations(observed)
+    plt.plot(wave, flux, label='original')
+    wave_rv, flux, delta_l, start_wave = read_raw_observations(observed[:-5]+'_rv.fits')
+    plt.plot(wave_rv, flux, label='RV cor=' + str(rv))
+    plt.title(str(observed))
+    plt.legend()
+    plt.show()
+    return
 
 if __name__ == '__main__':
-    import argparse
 
     args = argparse.ArgumentParser(description='Calculate for velocity shifts and correct them.')
     args.add_argument('observed', type=str, help='Spectrum')
-    args.add_argument('-t', '--template', type=str, help='Template', choices=['initial.spec', 'HARPS.Archive_Sun-4_norm.fits', 'HARPS.Archive_Arcturus_norm.fits', 'HARPS.GBOG_Procyon_norm.fits'], default='HARPS.Archive_Sun-4_norm.fits')
     args = args.parse_args()
 
-    template = 'HARPS.Archive_Sun-4_norm.fits'
-
-    dw, df, d = read_observations(args.observed, 4000, 6900)
-    plt.plot(dw, df)
-    plt.show()
-
-    tw, tf = read_template(template)
+    print(args.observed)
     dw, df = read_spec(args.observed)
-    # Plot template and data
-    plt.title("Template (blue) and data (red)")
-    plt.plot(tw, tf, 'b.-')
-    plt.plot(dw, df, 'r.-')
-    plt.show()
 
-    rv = velocity_shift(dw, df, tw, tf)
-    rv_correction(args.observed, round(rv,3))
+    template = 'data/HARPS.Archive_Sun-4_norm.fits'
+    tw, tf = read_template(template)
+
+    rv = velocity_shift(dw, df, tw, tf, plot=False)
+    # Save fits file
+    rv_correction(args.observed, round(rv, 2))
+    # plot this file onced it is saved!
+    # plot_rv(args.observed, rv)
