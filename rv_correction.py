e26f142ac118f597e26099212dfe3b8ec97fdd09
Maria Tsantaki <Maria.Tsantaki@astro.up.pt>
2025-03-30 22:26:54 +0200

    Update rv_correction.py
    catch bugs for wrong fortmat

--- rv_correction.py
+++ rv_correction.py
@@ -39,6 +39,9 @@ def read_template(fname):
 def read_spec(fname):
     """Reads and normalizes observed spectra."""
     SNR = snr(fname)
+    if SNR == None:
+        print('SNR is None')
+        return [np.nan], [np.nan]
     intervals = [5500., 6700.]
     dw, df, d = local_norm(fname, intervals, SNR)
     df = pyasl.instrBroadGaussFast(dw, df, BROADENING_RES, fullout=False, maxsig=None)
@@ -52,7 +55,7 @@ def velocity_shift(dw, df, tw, tf, name='lol', plot=False):
     # Find the index of maximum cross-correlation function
     maxind = np.argmax(cc)
 
-    if abs(rv[maxind])>199:
+    if abs(rv[maxind])>99.:
         # search Gaia DR3 for RV limits
         result_table = Simbad.query_object(name)
 
@@ -120,15 +123,17 @@ if __name__ == '__main__':
         name = observed.split('/')[-1].split('_')[0]
         print(f"Processing: {name}")
         dw, df = read_spec(observed)
+        if dw[0] == np.nan:
+            print('Something wrong with the spectra!')
         # # Plot template and data
         # plt.title("Template (blue) and data (red)")
         # plt.plot(dw, df)
         # plt.plot(tw, tf, 'b.-')
         # plt.show()
-
-        rv = velocity_shift(dw, df, tw, tf, name=name, plot=False)
-        # Apply RV correction and plot if significant
-        rv_correction(observed, round(rv, 2))
-        if abs(rv)>50:
-            # plot this file onced it is saved!
-            plot_rv(observed, rv)
+    else:
+            rv = velocity_shift(dw, df, tw, tf, name=name, plot=False)
+            # Apply RV correction and plot if significant
+            rv_correction(observed, round(rv, 2))
+            if abs(rv)>10:
+                # plot this file onced it is saved!
+                plot_rv(observed, rv)
